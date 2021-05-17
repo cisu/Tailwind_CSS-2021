40cac9a175f4c21d1966b94bbd3c21df38c8812f
cisu <49948070+cisu@users.noreply.github.com>
2021-05-17 21:00:32 +0300

    Responsive Nav (part 2)

---
+++ public/index.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById('menu');
+const burger = document.getElementById('burger');
+
+
+burger.addEventListener('click', () => {
+    if (menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    }
+    else{
+        menu.classList.add('hidden');
+    }
+})
